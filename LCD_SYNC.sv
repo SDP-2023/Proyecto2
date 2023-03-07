34c4a7960322d2a403780fb09200149de997529e
Arnau Mora <arnyminer.z@gmail.com>
2023-03-07 09:02:51 +0100

    Implementado LCD_SYNC

--- LCD_SYNC.sv
+++ LCD_SYNC.sv
@@ -1,8 +1,48 @@
 module LCD_SYNC(
     input CLK, RST_n,
     output reg NCLK, GREST, HD, DEN, VD,
-    output reg [11:0] Columna,
-    output reg [9:0] Fila
+    output reg [10:0] Columna,
+    output reg [8:0] Fila
 );
-    
+    wire HCOUNT_TC;
+    wire VCOUNT_TC;
+
+    pll_ltm pll_ltm_inst(
+        .inclk0(CLK),
+        .c0(NCLK)
+    );
+
+    count #(
+        .modulo(1056)
+    ) HCOUNT (
+        .CLK(NCLK),
+        .RSTn(RST_n),
+        .ENABLE(1'b1),
+        .UP_DOWN(1'b1),
+        .COUNT(Columna),
+        .TC(HCOUNT_TC)
+    );
+
+    count #(
+        .modulo(525)
+    ) VCOUNT (
+        .CLK(NCLK),
+        .RSTn(RST_n),
+        .ENABLE(HCOUNT_TC),
+        .UP_DOWN(1'b1),
+        .COUNT(Fila),
+        .TC(VCOUNT_TC)
+    );
+
+    GeneradorDen GEN_DEN (
+        .Columna(Columna),
+        .Fila(Fila),
+        .DEN(DEN)
+    );
+
+    assign HD = !HCOUNT_TC;
+    assign VD = !VCOUNT_TC;
+
+    assign GREST = RST_n;
+
 endmodule
