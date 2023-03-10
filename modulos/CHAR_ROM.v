988626d3eb5e91063e2d2d49217d94cf4c5b36bb
carlosviji10 <cviljim@teleco.upv.es>
2023-03-10 19:42:13 +0100

    Creación de la rom de los caract

---
+++ modulos/CHAR_ROM.v
@@ -0,0 +1,27 @@
+/** Crearemos ahora el módulo de la ROM de los caracteres 
+**/
+
+module CHAR_ROM(Filas, Columnas,NCLK,Data );
+// Declaramos las entradas -->
+input [9:0] Filas;
+input [10:0] Columnas;
+input NCLK;
+// Declaramos salidas -->
+output Data;
+// Declaramos un cable el cual será utilizado para las instancias -->
+wire [5:0] Address;
+// Declaramos un parámetro -->
+parameter Caracter=7'h30;
+// Instanciamos el módulo de direccionamiento creado en subtarea 3 -->
+direccionamiento DIR (
+	.Columnas(Columnas),
+	.Filas(Filas),
+	.Address(Address)
+);
+// Declaramos la instancia de la propia rom creada mediante el IP_catalog -->
+ROM_char	ROM_char_inst(
+	.address ( {Caracter,Address}),
+	.clock (NCLK),
+	.q (Data));
+	
+endmodule
