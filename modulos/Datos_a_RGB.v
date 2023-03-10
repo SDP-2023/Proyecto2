11c289448ce59c5346e391b608a41ae18b93f90a
carlosviji10 <cviljim@teleco.upv.es>
2023-03-10 19:16:44 +0100

    Comentando más cositas

--- modulos/Datos_a_RGB.v
+++ modulos/Datos_a_RGB.v
@@ -10,7 +10,10 @@ input [15:0] Data ; //16bits de tamaño.
 output reg [7:0] R, G, B ; // 8bits de tamaño cada una.
 // Declaración del bloque procedural always con "Data" como lista de sensibilidad --> 
 always @(Data)
+// En cuanto a la organización de la estructura de posición de color de cada pixel, lo organizamos siguiendo
+// La estructura propuesta en el propio guión de prácticas de la subtarea_3 -->
 begin
+    // Sabiendo que trabajamos con imagen de 16 bits, los 5 primeros para el rojo, lo 5 siguiente en verde y el resto a blue.
 	R={Data[15:11],Data[15:13]} ; 
 	G={Data[10:5],Data[10:9]} ;
 	B={Data[4:0],Data[4:2]} ; 
