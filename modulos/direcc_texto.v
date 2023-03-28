42f378e1bd27914ff8d7d82ac2c94577ff50d27c
carlosviji10 <cviljim@teleco.upv.es>
2023-03-28 11:15:21 +0200

    Terminando el tratado con el segundo dirrec

--- modulos/direcc_texto.v
+++ modulos/direcc_texto.v
@@ -10,16 +10,16 @@ module direcc_texto(Filas, Columnas, Address);
 input [9:0] Filas ;
 input [10:0] Columnas ;
 // Declaramos única salida -->
-output reg [16:0] Address ; // Como son 17 bits por la memoria utilizada... --> "[16:0]"
+output reg [5:0] Address ; // Como son 17 bits por la memoria utilizada... --> "[16:0]"
 // Declaramos registros para parametrizarlos según modo de direccionamiento usado.
 reg [8:0]n ;
 reg [7:0]m ;
 // Iniciamos bloque procedural "always" con entradas como lista de sensibilidad.
 always@(Columnas,Filas) 
 begin 
-      m = (Filas-35) ; // Renderización de las filas. --> Back Porch_Y = 35
-      n = (Columnas-216) ; // Renderización de las columnas. --> Back Porch_X = 216
-	  Address = {m,n} ; // Parametrizamos la dirección donde buscar en la ROM.
+      m = (Filas-35)/2 ; // Renderización de las filas. --> Back Porch_Y = 35
+      n = (Columnas-216)/2 ; // Renderización de las columnas. --> Back Porch_X = 216
+	  Address = {m[2:0],n[2:0]} ; // Parametrizamos la dirección donde buscar en la ROM.
 end
 
 endmodule 
