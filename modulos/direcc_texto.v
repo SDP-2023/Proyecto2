8439d4e673e6d809bd92f32b45c7db49770c265d
Arnau Mora <arnyminer.z@gmail.com>
2023-03-28 10:02:57 +0200

    Division por 2

--- modulos/direcc_texto.v
+++ modulos/direcc_texto.v
@@ -7,18 +7,18 @@
 **/
 module direcc_texto(Filas, Columnas, Address);
 // Declaramos entradas -->
-input [7:0] Filas ;
-input [7:0] Columnas ;
+input [9:0] Filas ;
+input [10:0] Columnas ;
 // Declaramos única salida -->
 output reg [16:0] Address ; // Como son 17 bits por la memoria utilizada... --> "[16:0]"
 // Declaramos registros para parametrizarlos según modo de direccionamiento usado.
-reg [3:0]n ;
-reg [3:0]m ;
+reg [8:0]n ;
+reg [7:0]m ;
 // Iniciamos bloque procedural "always" con entradas como lista de sensibilidad.
 always@(Columnas,Filas) 
 begin 
-      m = (Filas-35)/2 ; // Renderización de las filas. --> Back Porch_Y = 35
-      n = (Columnas-216)/2 ; // Renderización de las columnas. --> Back Porch_X = 216
+      m = (Filas-35) ; // Renderización de las filas. --> Back Porch_Y = 35
+      n = (Columnas-216) ; // Renderización de las columnas. --> Back Porch_X = 216
 	  Address = {m,n} ; // Parametrizamos la dirección donde buscar en la ROM.
 end
 
