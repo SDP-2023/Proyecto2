6f191ba3b10bdafde37c5e0f858115da402ad87a
Arnau Mora <arnyminer.z@gmail.com>
2023-03-28 10:00:04 +0200

    Cambiado selector de color a assign

--- modulos/SELEC_COLOR.v
+++ modulos/SELEC_COLOR.v
@@ -1,28 +1,23 @@
 /** Crearemos en este fichero el diseño a realizar para la selección de color en pantalla. 
 **/
 
-module SELEC_COLOR(Seleccion_color, R, G, B);
+module SELEC_COLOR #(
+	parameter BG_R = 8'b00000000,
+	parameter BG_G = 8'b11111111,
+	parameter BG_B = 8'b00000000,
+	parameter FG_R = 8'b01111111,
+	parameter FG_G = 8'b01111111,
+	parameter FG_B = 8'b00000000
+)(Seleccion_color, R, G, B);
 // Entradas -->
 input Seleccion_color ;
 // Salidas como registros -->
-output reg [0:7] R, G, B ;
-// Generamos el bloque procedural always con entrada definida arriba como lista de sensibilidad -->
-always @(Seleccion_color)
-begin
-	// Si está la entrada a nivel bajo, entonces se establece de color predeterminado
-	if(Seleccion_color==1'b0)
-		begin
-			R<=8'b00000000 ;
-			G<=8'b11111111 ;
-			B<=8'b00000000 ;			
-		end
-		// Si la entrada está a nivel alto cambiamos el registro de nivel de color RGB
-	else
-		begin
-			R<=8'b01111111 ;
-			G<=8'b01111111 ;
-			B<=8'b00000000 ;			
-		end
-end
+output [0:7] R, G, B ;
+
+// Si la entrada está a nivel alto cambiamos el registro de nivel de color RGB
+// Si está la entrada a nivel bajo, entonces se establece de color predeterminado
+assign R = Seleccion_color ? FG_R : BG_R;
+assign G = Seleccion_color ? FG_G : BG_G;
+assign B = Seleccion_color ? FG_B : BG_B;
 
 endmodule 
